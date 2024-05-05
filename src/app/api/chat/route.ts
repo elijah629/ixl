4c744b5d0e2ff0ba7bdf148deea1d31de0e6c8ff
Elijah629 <62805599+elijah629@users.noreply.github.com>
2024-05-04 19:00:23 -0700

    add: bookmarklet version, fix: make model's output use less tokens (max:10), format

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,31 +1,45 @@
-import { createOpenAI } from '@ai-sdk/openai'
-import { generateText, tool } from 'ai'
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateText, tool } from "ai";
 import { z } from "zod";
 
 const groq = createOpenAI({
   apiKey: process.env.GROQ_API_KEY || "",
   baseURL: "https://api.groq.com/openai/v1",
-})
+});
+
+const system = `Provide the correct solution(s) to the given problem using the 'must_use_for_solution' tool. If the question has a single correct answer, return that answer. If the question has multiple correct answers, return a list of correct answers. Please only return the letter of the answer. 
+Example:
+Which point of view does the narrator use in the passage?
+My Aunt Helen was my favorite person in the whole world. She was my mom's sister. She got straight A's when she was a teenager, and she used to give me books to read. My father said that the books were a little too old for me, but I liked them, so he just shrugged and let me read.
+a) first person
+b) second person
+c) third person
+Correct response:
+a
+`;
 
 export async function POST(req: Request) {
-  const { prompt }: { prompt: string } = await req.json()
+  const { prompt }: { prompt: string } = await req.json();
   const model = groq("llama3-70b-8192");
   const result = await generateText({
-		model,
-		temperature: 0.4,
-		system: `Please provide only the correct solution to the given problem using the "must_use_for_solution" tool. If the question requires a single answer, return that answer. If the question requires multiple answers, return a list of correct answers.`,
-		prompt,
-		tools: {
-			must_use_for_solution: tool({
-				description: "Must be used to return the solution to the problem.",
-			    parameters: z.object({
-				     answers: z.array(z.string().describe("A correct answer")).describe("List of correct answers")
-    			}),
-				execute: async ({ answers }) => answers
-			}),
-		}
-  })
-  
+    model,
+    temperature: 0.4,
+    maxTokens: 10,
+    system,
+    prompt,
+    tools: {
+      must_use_for_solution: tool({
+        description: "Must be used to return the solution to the problem.",
+        parameters: z.object({
+          answers: z
+            .array(z.string().describe("A correct answer"))
+            .describe("List of correct answers"),
+        }),
+        execute: async ({ answers }) => answers,
+      }),
+    },
+  });
+
   const tools = result.toolResults;
   const answers: string[] = tools[0]?.result ?? [result?.text];
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   title: "IXL AI",
 };
 
+const v = "1.0.1";
 const bookmarklet = `const n=Array.from(document.querySelector(".ixl-practice-crate").children[1].children).map(n=>n.innerText);n[0]+="\\n";n[n.length-1]=n[n.length-1].split("\\n").map((n,e)=>String.fromCharCode(97 + e)+") "+n).join("\\n");await navigator.clipboard.writeText(n.join("\\n"));`;
 
 export default function RootLayout({
@@ -18,17 +19,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx(jbm.className, "bg-white dark:bg-black", "text-black dark:text-white", "flex", "flex-col")}>
-		<header>
-			<nav className="border-b-black dark:border-b-white border-b-[1px] min-h-12 flex items-center px-4 gap-2">
-				<span className="font-bold">IXL AI</span>
-				<a href={`javascript:!async function(){${bookmarklet}}()`} className="rounded-md bg-black dark:bg-white text-white dark:text-black p-1" title="Drag into bookmarks bar to install, if you do not see a bookmarks bar you may need to enable it">Bookmarklet</a>
-			</nav>
-		</header>
-		<main className="flex-1">
-			{children}
-		</main>
-	</body>
+      <body
+        className={clsx(
+          jbm.className,
+          "bg-white dark:bg-black",
+          "text-black dark:text-white",
+          "flex",
+          "flex-col",
+        )}
+      >
+        <header>
+          <nav className="border-b-black dark:border-b-white border-b-[1px] min-h-12 flex items-center px-4 gap-4">
+            <span className="font-bold">IXL AI</span>
+            <a
+              href={`javascript:!async function(){${bookmarklet}}()`}
+              className="rounded-md bg-black dark:bg-white text-white dark:text-black p-1"
+              title="Drag into bookmarks bar to install, if you do not see a bookmarks bar you may need to enable it"
+            >
+              IXL AI Bookmarklet v{v}
+            </a>
+          </nav>
+        </header>
+        <main className="flex-1">{children}</main>
+      </body>
     </html>
   );
 }
